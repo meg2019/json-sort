e3267ddbcc87e4ef69bee595976d53b20ee2e500
Victor <megazoid@protonmail.com>
2022-04-19 08:15:14 +0300

    First commit, implementing all functions

---
+++ src/main/java/telran/b7a/controller/JsonSortAppl.java
@@ -0,0 +1,41 @@
+package telran.b7a.controller;
+
+import java.io.File;
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonSortAppl {
+
+	public static void main(String[] args) throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, Object>jsonData = objectMapper.readValue(new URL("file:target/BA.json"), TreeMap.class);
+//		jsonData.entrySet().forEach(System.out::println);
+		sortField(jsonData);
+		objectMapper.writeValue(new File("target/BA_sorted.json"), jsonData);
+		
+	}
+
+	private static void sortField(Map<String, Object> jsonData) {
+		Set<Entry<String, Object>> entries = jsonData.entrySet(); 
+		for (Entry<String, Object> entry : entries) {
+			if (entry.getValue() instanceof Map<?, ?>) {
+				entry.setValue(new TreeMap<String, Object>((Map<String, Object>) entry.getValue()));
+			}
+			if (entry.getValue() instanceof List<?>) {
+				List<?> list = (List<?>) entry.getValue();
+				if (list.size() > 0) {
+					list.forEach(e -> sortField((Map<String, Object>)e));
+				}
+			}
+			
+		}
+	}
+
+}
